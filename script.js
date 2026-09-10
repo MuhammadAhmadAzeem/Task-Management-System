8d5ca393f425912bfee31b4b55373efd129e5504
MuhammadAhmadAzeem <ahmadazeem8073@gmail.com>
2026-09-10 14:31:59 +0500

    Create task management system

---
+++ script.js
@@ -0,0 +1,459 @@
+let btn = document.querySelector("#addBtn");
+let inp = document.querySelector("#addTask");
+let description = document.querySelector("#description");
+
+let category = document.querySelector("#category");
+let priority = document.querySelector("#priority");
+let dueDate = document.querySelector("#dueDate");
+
+let search = document.querySelector("#search");
+let statusFilter = document.querySelector("#statusFilter");
+let priorityFilter = document.querySelector("#priorityFilter");
+
+let ul = document.querySelector("#taskList");
+
+let total = document.querySelector("#total");
+let pending = document.querySelector("#pending");
+let progress = document.querySelector("#progress");
+let completed = document.querySelector("#completed");
+
+
+// Update ke liye variable
+let update = null;
+
+
+// Tasks array
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+// =========================
+// Add Task
+// =========================
+
+btn.addEventListener("click", function () {
+
+    if (inp.value.trim() === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+
+    // Agar update mode hai
+    if (update) {
+
+        update.querySelector(".task-title").textContent = inp.value;
+
+        update.querySelector(".task-description").textContent =
+            description.value;
+
+        update.querySelector(".category").textContent =
+            category.value;
+
+        update.querySelector(".priority").textContent =
+            priority.value;
+
+        update.querySelector(".due-date").textContent =
+            dueDate.value;
+
+        update = null;
+
+        btn.innerText = "Add Task";
+
+        inp.value = "";
+        description.value = "";
+        dueDate.value = "";
+
+        return;
+    }
+
+
+    // New Task Object
+
+    let task = {
+
+        id: Date.now(),
+
+        title: inp.value,
+
+        description: description.value,
+
+        category: category.value,
+
+        priority: priority.value,
+
+        status: "Pending",
+
+        dueDate: dueDate.value
+
+    };
+
+
+    tasks.push(task);
+
+    saveTasks();
+
+    showTasks();
+
+
+    // Clear Inputs
+
+    inp.value = "";
+    description.value = "";
+    dueDate.value = "";
+
+});
+
+
+// =========================
+// Show Tasks
+// =========================
+
+function showTasks() {
+
+    ul.innerHTML = "";
+
+
+    let searchValue = search.value.toLowerCase();
+
+    let statusValue = statusFilter.value;
+
+    let priorityValue = priorityFilter.value;
+
+
+    tasks.forEach(function (task) {
+
+        // Search
+        if (!task.title.toLowerCase().includes(searchValue)) {
+            return;
+        }
+
+
+        // Status Filter
+        if (
+            statusValue !== "All" &&
+            task.status !== statusValue
+        ) {
+            return;
+        }
+
+
+        // Priority Filter
+        if (
+            priorityValue !== "All" &&
+            task.priority !== priorityValue
+        ) {
+            return;
+        }
+
+
+        // LI
+
+        let li = document.createElement("li");
+
+
+        // Task Information
+
+        let taskInfo = document.createElement("div");
+
+        taskInfo.classList.add("task-info");
+
+
+        let title = document.createElement("div");
+
+        title.classList.add("task-title");
+
+        title.innerText = task.title;
+
+
+        let desc = document.createElement("div");
+
+        desc.classList.add("task-description");
+
+        desc.innerText = task.description;
+
+
+        let details = document.createElement("div");
+
+        details.classList.add("task-details");
+
+        details.innerHTML = `
+            Category:
+            <span class="category">${task.category}</span>
+
+            |
+
+            Priority:
+            <span class="priority ${task.priority.toLowerCase()}">
+                ${task.priority}
+            </span>
+
+            |
+
+            Due:
+            <span class="due-date">
+                ${task.dueDate || "No Date"}
+            </span>
+        `;
+
+
+        // Status
+
+        let status = document.createElement("span");
+
+        status.classList.add("status");
+
+        if (task.status === "Pending") {
+
+            status.classList.add("pending");
+
+        } else if (task.status === "In Progress") {
+
+            status.classList.add("in-progress");
+
+        } else {
+
+            status.classList.add("completed-status");
+
+        }
+
+        status.innerText = task.status;
+
+
+        // Update Button
+
+        let updateBtn = document.createElement("button");
+
+        updateBtn.innerText = "Update";
+
+        updateBtn.classList.add("update");
+
+
+        // Delete Button
+
+        let delBtn = document.createElement("button");
+
+        delBtn.innerText = "Delete";
+
+        delBtn.classList.add("delete");
+
+
+        // Complete Button
+
+        let completeBtn = document.createElement("button");
+
+        completeBtn.innerText = "Complete";
+
+        completeBtn.classList.add("complete");
+
+
+        // Append
+
+        taskInfo.appendChild(title);
+
+        taskInfo.appendChild(desc);
+
+        taskInfo.appendChild(details);
+
+        li.appendChild(taskInfo);
+
+        li.appendChild(status);
+
+        li.appendChild(completeBtn);
+
+        li.appendChild(delBtn);
+
+        li.appendChild(updateBtn);
+
+        ul.appendChild(li);
+
+
+        // Task ID save karna
+
+        li.dataset.id = task.id;
+
+    });
+
+
+    updateCounters();
+
+}
+
+
+// =========================
+// Delete / Update / Complete
+// =========================
+
+ul.addEventListener("click", function (event) {
+
+    if (event.target.nodeName === "BUTTON") {
+
+
+        let listItems = event.target.parentElement;
+
+        let id = Number(listItems.dataset.id);
+
+
+        // Delete
+
+        if (event.target.innerText === "Delete") {
+
+            tasks = tasks.filter(function (task) {
+
+                return task.id !== id;
+
+            });
+
+            saveTasks();
+
+            showTasks();
+
+        }
+
+
+        // Update
+
+        if (event.target.innerText === "Update") {
+
+            update = listItems;
+
+
+            inp.value =
+                listItems.querySelector(".task-title").textContent;
+
+
+            description.value =
+                listItems.querySelector(".task-description").textContent;
+
+
+            category.value =
+                listItems.querySelector(".category").textContent;
+
+
+            priority.value =
+                listItems.querySelector(".priority").textContent;
+
+
+            dueDate.value =
+                listItems.querySelector(".due-date").textContent;
+
+
+            btn.innerText = "Update";
+
+        }
+
+
+        // Complete
+
+        if (event.target.innerText === "Complete") {
+
+            tasks.forEach(function (task) {
+
+                if (task.id === id) {
+
+                    task.status = "Completed";
+
+                }
+
+            });
+
+            saveTasks();
+
+            showTasks();
+
+        }
+
+    }
+
+});
+
+
+// =========================
+// Search
+// =========================
+
+search.addEventListener("input", function () {
+
+    showTasks();
+
+});
+
+
+// =========================
+// Status Filter
+// =========================
+
+statusFilter.addEventListener("change", function () {
+
+    showTasks();
+
+});
+
+
+// =========================
+// Priority Filter
+// =========================
+
+priorityFilter.addEventListener("change", function () {
+
+    showTasks();
+
+});
+
+
+// =========================
+// Counters
+// =========================
+
+function updateCounters() {
+
+    total.innerText = tasks.length;
+
+
+    let pendingTasks = tasks.filter(function (task) {
+
+        return task.status === "Pending";
+
+    });
+
+    pending.innerText = pendingTasks.length;
+
+
+    let progressTasks = tasks.filter(function (task) {
+
+        return task.status === "In Progress";
+
+    });
+
+    progress.innerText = progressTasks.length;
+
+
+    let completedTasks = tasks.filter(function (task) {
+
+        return task.status === "Completed";
+
+    });
+
+    completed.innerText = completedTasks.length;
+
+}
+
+
+// =========================
+// Local Storage
+// =========================
+
+function saveTasks() {
+
+    localStorage.setItem(
+        "tasks",
+        JSON.stringify(tasks)
+    );
+
+}
+
+
+// =========================
+// First Load
+// =========================
+
+showTasks();
